62e860d01398e8997bac5fcac5a9b5df0e1cd9de
David Abol Ger <draker74@gmail.com>
2022-06-22 15:36:06 +0300

    All content

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import {
+  Card, Row, Col, Input,
+} from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 function CoinDetails() {
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  console.log(cryptos);
   return (
-    <div>
-      <h1>Coin Details</h1>
-    </div>
+    <>
+      <Row gutter={[28, 28]} className="crypto-card-container">
+        {cryptos.map((crypto) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+            <Link to={`/crypto/${crypto.id}`}>
+              <Card
+                title={`${crypto.rank}.${crypto.name}`}
+                extra={<img className="crypto-image" src={crypto.iconUrl} style={{ width: 48 }} />}
+                hoverable
+
+              >
+
+                <p>
+                  Price
+                  {millify(crypto.price)}
+                </p>
+                <p>
+                  Market Cap:
+                  {millify(crypto.marketCap)}
+                </p>
+                <p>
+                  Daily Change:
+                  {millify(crypto.change)}
+                  %
+                </p>
+              </Card>
+
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,37 @@ import {
   Typography, Row, Col, Statistic,
 } from 'antd';
 import millify from 'millify';
+import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import About from './About';
+import CoinDetails from './CoinDetails';
 
 const { Title } = Typography;
 
 const Home = () => {
-  const { data } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery();
   const globalStats = data?.data?.stats;
-  console.log(data);
+  if (isFetching) return <div>Loading...</div>;
   return (
     <>
       <Title level={2} className="heading"> Ger Crypto Stats</Title>
       <Row>
         <Col span={12}><Statistic title="All Crypto Currencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="All Crypto Currencies" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="All Crypto Currencies" value="5" /></Col>
-        <Col span={12}><Statistic title="All Crypto Currencies" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total Volume" value={millify(globalStats.total24hVolume)} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title"> Top 10 Cryptocurrencies</Title>
+        <Title level={3} className="show-more"><Link to="/CoinDetails">Show More Coins...</Link></Title>
+      </div>
+      <CoinDetails simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title"> Latest Cryptocurrencies News</Title>
+        <Title level={3} className="show-more"><Link to="/about">Show More Coins...</Link></Title>
+      </div>
+      <About simplified />
     </>
   );
 };
